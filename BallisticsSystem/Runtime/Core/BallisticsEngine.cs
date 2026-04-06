1e115ff591f9550d10fbf91e8111b3086fb5e4ea
SorrowsLee <sorrowslee@SorrowsLeedeMacBook-Pro.local>
2026-04-06 11:54:23 +0800

    修正新增武器後，沒有更換外型的問題

--- BallisticsSystem/Runtime/Core/BallisticsEngine.cs
+++ BallisticsSystem/Runtime/Core/BallisticsEngine.cs
@@ -5,23 +5,29 @@ namespace Sorrows.Ballistics
 {
     public static class BallisticsEngine
     {
-        public static BulletInstance Spawn(ProjectileData def, GameObject prefab, Vector2 position, Vector2 direction, LayerMask collisionMask, LayerMask pierceableLayers = default, LayerMask nonBounceLayers = default, Action<BulletInstance, GameObject, RaycastHit2D> onHit = null)
+        public static BulletInstance Spawn(ProjectileData def, GameObject prefab, Vector2 position, Vector2 direction, LayerMask collisionMask, LayerMask pierceableLayers = default, LayerMask nonBounceLayers = default, Action<BulletInstance, GameObject, RaycastHit2D> onHit = null, Sprite bulletSprite = null)
         {
-            return Internal_Create(def, prefab, position, direction, collisionMask, pierceableLayers, nonBounceLayers, onHit);
+            return Internal_Create(def, prefab, position, direction, collisionMask, pierceableLayers, nonBounceLayers, onHit, bulletSprite);
         }
 
         internal static void Internal_SpawnSplit(ProjectileData def, GameObject prefab, Vector2 position, Vector2 direction, LayerMask collisionMask, LayerMask pierceableLayers = default, LayerMask nonBounceLayers = default, Action<BulletInstance, GameObject, RaycastHit2D> onHit = null)
         {
-            Internal_Create(def, prefab, position, direction, collisionMask, pierceableLayers, nonBounceLayers, onHit);
+            Internal_Create(def, prefab, position, direction, collisionMask, pierceableLayers, nonBounceLayers, onHit, null);
         }
 
-        private static BulletInstance Internal_Create(ProjectileData def, GameObject prefab, Vector2 position, Vector2 direction, LayerMask collisionMask, LayerMask pierceableLayers, LayerMask nonBounceLayers, Action<BulletInstance, GameObject, RaycastHit2D> onHit)
+        private static BulletInstance Internal_Create(ProjectileData def, GameObject prefab, Vector2 position, Vector2 direction, LayerMask collisionMask, LayerMask pierceableLayers, LayerMask nonBounceLayers, Action<BulletInstance, GameObject, RaycastHit2D> onHit, Sprite bulletSprite)
         {
             GameObject go = UnityEngine.Object.Instantiate(prefab, position, Quaternion.identity);
             BulletInstance instance = go.GetComponent<BulletInstance>();
             if (instance != null)
             {
                 if (onHit != null) instance.OnBulletHitObject += onHit;
+
+                if (bulletSprite != null)
+                {
+                    SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+                    if (sr != null) sr.sprite = bulletSprite;
+                }
 
                 instance.Velocity = direction.normalized * def.Speed;
                 instance.Radius = def.Radius;
